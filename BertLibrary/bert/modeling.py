f757a4e10b4733df4a27c7ba91a7db991ecb1593
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2022-02-22 22:18:15 +0000

    Script changes

--- BertLibrary/bert/modeling.py
+++ BertLibrary/bert/modeling.py
@@ -10,4 +10,17 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing 
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The main BERT model and related functions."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import copy
+import json
+import math
+import re
+import
