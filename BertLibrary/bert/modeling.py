03e7c9f05c8024c5c9e94f6c2c4e9f27621b7bbb
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2022-04-01 16:32:50 +0000

    howto

--- BertLibrary/bert/modeling.py
+++ BertLibrary/bert/modeling.py
@@ -225,4 +225,11 @@ class BertModel(object):
         # We "pool" the model by simply taking the hidden state corresponding
         # to the first token. We assume that this has been pre-trained
         first_token_tensor = tf.squeeze(self.sequence_output[:, 0:1, :], axis=1)
-     
+        self.pooled_output = tf.layers.dense(
+            first_token_tensor,
+            config.hidden_size,
+            activation=tf.tanh,
+            kernel_initializer=create_initializer(config.initializer_range))
+
+  def get_pooled_output(self):
+    return self.
