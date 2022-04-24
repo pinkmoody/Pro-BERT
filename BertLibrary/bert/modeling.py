d674b0f756146f6eaa7b2554eac34b71f48bc80f
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2022-04-24 10:22:22 +0000

    Update lib.php

--- BertLibrary/bert/modeling.py
+++ BertLibrary/bert/modeling.py
@@ -335,4 +335,15 @@ def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
     if name not in name_to_variable:
       continue
     assignment_map[name] = name
-    initialized_variable_names[
+    initialized_variable_names[name] = 1
+    initialized_variable_names[name + ":0"] = 1
+
+  return (assignment_map, initialized_variable_names)
+
+
+def dropout(input_tensor, dropout_prob):
+  """Perform dropout.
+
+  Args:
+    input_tensor: float Tensor.
+    dropout_prob: Python float. The probab
