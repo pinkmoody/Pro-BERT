e0ca98287f1725a0b1afb8e8f1d0d7673b950c01
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2022-04-24 13:50:07 +0000

    Implementation

--- BertLibrary/bert/modeling.py
+++ BertLibrary/bert/modeling.py
@@ -346,4 +346,13 @@ def dropout(input_tensor, dropout_prob):
 
   Args:
     input_tensor: float Tensor.
-    dropout_prob: Python float. The probab
+    dropout_prob: Python float. The probability of dropping out a value (NOT of
+      *keeping* a dimension as in `tf.nn.dropout`).
+
+  Returns:
+    A version of `input_tensor` with dropout applied.
+  """
+  if dropout_prob is None or dropout_prob == 0.0:
+    return input_tensor
+
+  output = tf.n
