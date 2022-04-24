27fd88f78349bda23823748fc8060dd107b62137
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2022-04-24 08:10:46 +0000

    Update CodeBook.MD

--- BertLibrary/bert/modeling.py
+++ BertLibrary/bert/modeling.py
@@ -325,4 +325,14 @@ def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
     m = re.match("^(.*):\\d+$", name)
     if m is not None:
       name = m.group(1)
-    name
+    name_to_variable[name] = var
+
+  init_vars = tf.train.list_variables(init_checkpoint)
+
+  assignment_map = collections.OrderedDict()
+  for x in init_vars:
+    (name, var) = (x[0], x[1])
+    if name not in name_to_variable:
+      continue
+    assignment_map[name] = name
+    initialized_variable_names[
