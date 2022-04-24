7c64e641402b8d3ce14bf73ea72d64a8f43f997a
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2022-04-24 06:26:42 +0000

    updated gem

--- BertLibrary/bert/modeling.py
+++ BertLibrary/bert/modeling.py
@@ -315,4 +315,14 @@ def get_activation(activation_string):
 
 
 def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
-  """Compute the union of 
+  """Compute the union of the current variables and checkpoint variables."""
+  assignment_map = {}
+  initialized_variable_names = {}
+
+  name_to_variable = collections.OrderedDict()
+  for var in tvars:
+    name = var.name
+    m = re.match("^(.*):\\d+$", name)
+    if m is not None:
+      name = m.group(1)
+    name
