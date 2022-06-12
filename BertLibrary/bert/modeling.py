8a49ae3d4f14e58032e192efde680f2ac2610e0d
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2022-06-12 07:48:13 +0000

    Update scrutinizer config

--- BertLibrary/bert/modeling.py
+++ BertLibrary/bert/modeling.py
@@ -525,4 +525,11 @@ def create_attention_mask_from_input_mask(from_tensor, to_mask):
   """Create 3D attention mask from a 2D tensor mask.
 
   Args:
-    from_tensor: 2D or 3D Tensor of shape [batch_siz
+    from_tensor: 2D or 3D Tensor of shape [batch_size, from_seq_length, ...].
+    to_mask: int32 Tensor of shape [batch_size, to_seq_length].
+
+  Returns:
+    float Tensor of shape [batch_size, from_seq_length, to_seq_length].
+  """
+  from_shape = get_shape_list(from_tensor, expected_rank=[2, 3])
+  batch_size = from_shape[0
