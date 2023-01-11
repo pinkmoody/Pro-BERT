b2636bb691b2ccb3fb2cccd302589d0f5da6a948
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-01-11 09:05:51 +0000

    updates to docs

--- BertLibrary/bert/modeling.py
+++ BertLibrary/bert/modeling.py
@@ -875,4 +875,11 @@ def transformer_model(input_tensor,
         layer_output = tf.layers.dense(
             intermediate_output,
             hidden_size,
-            kernel_initializer=create
+            kernel_initializer=create_initializer(initializer_range))
+        layer_output = dropout(layer_output, hidden_dropout_prob)
+        layer_output = layer_norm(layer_output + attention_output)
+        prev_output = layer_output
+        all_layer_outputs.append(layer_output)
+
+  if do_return_all_layers:
+    fina
