f2db9774c7ed3eadeb2a543b133a4fcdeba7441f
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-05-20 09:22:33 +0000

    Small icon

--- BertLibrary/bert/run_classifier_with_tfhub.py
+++ BertLibrary/bert/run_classifier_with_tfhub.py
@@ -1,4 +1,11 @@
 # coding=utf-8
 # Copyright 2018 The Google AI Language Team Authors.
 #
-# Licensed under the Apache License, Version 2
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License
