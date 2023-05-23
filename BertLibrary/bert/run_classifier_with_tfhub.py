a75cb4d4a975789b3d1af91466b4526d6a84d06f
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-05-23 05:25:41 +0000

    New models

--- BertLibrary/bert/run_classifier_with_tfhub.py
+++ BertLibrary/bert/run_classifier_with_tfhub.py
@@ -14,4 +14,17 @@
 # limitations under the License.
 """BERT finetuning runner with TF-Hub."""
 
-from __
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import optimization
+import run_classifier
+import tokenization
+import tensorflow as tf
+import tensorflow_hub as hub
+
+flags = tf.flags
+
+FLAGS = flags.FLAGS
