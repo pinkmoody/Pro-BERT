6c6a4b5b4fcab5bb6984489f71dc63a24849467f
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-05-29 06:34:30 +0000

    Release v0.4.1

--- BertLibrary/bert/run_classifier_with_tfhub.py
+++ BertLibrary/bert/run_classifier_with_tfhub.py
@@ -69,4 +69,11 @@ def create_model(is_training, input_ids, input_mask, segment_ids, labels,
   with tf.variable_scope("loss"):
     if is_training:
       # I.e., 0.1 dropout
-      output_layer = tf.nn.dropout(output_layer, 
+      output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
+
+    logits = tf.matmul(output_layer, output_weights, transpose_b=True)
+    logits = tf.nn.bias_add(logits, output_bias)
+    probabilities = tf.nn.softmax(logits, axis=-1)
+    log_probs = tf.nn.log_softmax(logits, axis=-1)
+
+    one_hot_labels = tf.one
