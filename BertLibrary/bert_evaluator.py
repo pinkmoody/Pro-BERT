746ac8e264fa6b2a382f34b718cae9186c5052c6
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-08-15 22:48:04 +0000

    pipe: iovec: Fix memory corruption when retrying atomic copy as non-atomic

--- BertLibrary/bert_evaluator.py
+++ BertLibrary/bert_evaluator.py
@@ -9,4 +9,10 @@ class BertEvaluator:
     def __init__(self, model, iter_steps=1000):
         self.model = model
         self.processor = Label2TextProcessor(self.model.max_seq_len)
-        self.logging_hook = LoggingSessionHook(self.model, iter_s
+        self.logging_hook = LoggingSessionHook(self.model, iter_steps)
+
+    def convert_features(self, data_path, output_file):
+        examples = self.processor.file_get_examples(data_path)
+        file_based_convert_examples_to_features(examples,
+                                                self.model.labels,
+                                            
