e5558943389c7326b406dd4b92737433e5aa8219
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-08-15 20:14:59 +0000

    Move tree-widget/ from etc/ to etc/images/

--- BertLibrary/bert_evaluator.py
+++ BertLibrary/bert_evaluator.py
@@ -2,4 +2,11 @@ from .processors.Label2TextProcessor import Label2TextProcessor
 import os
 
 import tensorflow as tf
-from BertLibrary.bert
+from BertLibrary.bert.run_classifier import *
+
+class BertEvaluator:
+
+    def __init__(self, model, iter_steps=1000):
+        self.model = model
+        self.processor = Label2TextProcessor(self.model.max_seq_len)
+        self.logging_hook = LoggingSessionHook(self.model, iter_s
