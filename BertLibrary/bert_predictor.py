3bf1c4edeb4c13494d5108c93e8595cce19f1b98
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-08-28 11:44:39 +0000

    more refactors

--- BertLibrary/bert_predictor.py
+++ BertLibrary/bert_predictor.py
@@ -5,4 +5,11 @@ import numpy as np
 
 class BertPredictor:
 
-    def __init__(self, model_estima
+    def __init__(self, model_estimator, processor, config):
+        self.processor = processor
+        self.predictor = tf.contrib.predictor.from_estimator(
+            model_estimator, processor.serving_input_receiver_fn(), config=config)
+        self.bs = self.processor.batch_size
+
+    def __call__(self, sentences):
+    
