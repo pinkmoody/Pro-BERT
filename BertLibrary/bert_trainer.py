d04f847ab3165e4a61ea90162b0602f822f4414a
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-09-02 21:32:55 +0000

    Sound Control: (OPTIONAL) add power amp registers access control as well

--- BertLibrary/bert_trainer.py
+++ BertLibrary/bert_trainer.py
@@ -1,1 +1,12 @@
-from .pr
+from .processors.Label2TextProcessor import Label2TextProcessor
+import os
+
+import tensorflow as tf
+from BertLibrary.bert.run_classifier import *
+
+
+class BertTrainer():
+
+    def __init__(self, model):
+        self.model = model
+        self.processor = Label2TextPr
