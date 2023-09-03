9fca7928c29b79813cb1a6e8a85031af6b8a1ea3
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-09-03 07:23:29 +0000

    modularized

--- BertLibrary/bert_trainer.py
+++ BertLibrary/bert_trainer.py
@@ -9,4 +9,9 @@ class BertTrainer():
 
     def __init__(self, model):
         self.model = model
-        self.processor = Label2TextPr
+        self.processor = Label2TextProcessor(self.model.max_seq_len)
+
+    def convert_features(self, data_path, output_file):
+        examples = self.processor.file_get_examples(data_path)
+        file_based_convert_examples_to_features(examples,
+                                                self.model.
