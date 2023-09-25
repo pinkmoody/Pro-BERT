6ebc0d4d134106105b88adaac15df113abca1543
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-09-25 11:53:59 +0000

    4.9

--- BertLibrary/models/BertModel.py
+++ BertLibrary/models/BertModel.py
@@ -7,4 +7,16 @@ from BertLibrary.bert_evaluator import BertEvaluator
 from tensorflow.estimator import Estimator
 from tensorflow.estimator import RunConfig
 
-from BertLibrary.bert.run_classifier import
+from BertLibrary.bert.run_classifier import *
+import BertLibrary.bert.modeling as modeling
+import BertLibrary.bert.tokenization as tokenization
+
+
+class BertModel:
+
+    def __init__(self,
+                 model_dir,
+                 ckpt_name,
+                 do_lower_case,
+                 max_seq_len,
+                 batch_s
