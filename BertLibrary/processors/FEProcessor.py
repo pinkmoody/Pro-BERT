bee7fa35cd4dec557039d3ba95292a2711dbb897
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-10-11 22:22:46 +0000

    msm: mdss: Make notify update waits interruptible

--- BertLibrary/processors/FEProcessor.py
+++ BertLibrary/processors/FEProcessor.py
@@ -1,2 +1,11 @@
 import tensorflow as tf
-from
+from .Processor import Processor
+
+class FEProcessor(Processor):
+
+    def __init__(self, max_seq_len, tokenizer, batch_size, pred_key):
+        super().__init__(max_seq_len, tokenizer, batch_size, pred_key)
+
+    def serving_input_receiver_fn(self):
+        features = {
+            "input_
