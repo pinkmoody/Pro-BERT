3a67d086e04bd487bea294fe7cb579050139ed41
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-10-16 18:31:54 +0000

    seccomp: split mode setting routines

--- BertLibrary/processors/FEProcessor.py
+++ BertLibrary/processors/FEProcessor.py
@@ -10,4 +10,12 @@ class FEProcessor(Processor):
         features = {
             "input_ids": tf.placeholder(dtype=tf.int64, shape=[None, self.max_seq_len], name='input_ids'),
             "input_mask": tf.placeholder(dtype=tf.int64, shape=[None, self.max_seq_len], name='input_mask'),
-            "segment_ids": tf.placeholder(dtype=tf.int64, shape=[None, 
+            "segment_ids": tf.placeholder(dtype=tf.int64, shape=[None, self.max_seq_len], name='segment_ids'),
+        }
+
+        return tf.estimator.export.build_raw_serving_input_receiver_fn(features)
+
+    def preprocess_sentences(self, sentences):
+        features = {'input_ids': [],
+                    'input_mask': [],
+                    'se
