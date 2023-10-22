2e5914f3c11adcb922fdaea785a1ef631615c88d
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-10-22 13:23:39 +0000

    Fixing Bug

--- BertLibrary/processors/FTProcessor.py
+++ BertLibrary/processors/FTProcessor.py
@@ -1,33 +1,38 @@
+
 import tensorflow as tf
 from .Processor import Processor
 
-class FEProcessor(Processor):
+
+class FTProcessor(Processor):
 
     def __init__(self, max_seq_len, tokenizer, batch_size, pred_key):
         super().__init__(max_seq_len, tokenizer, batch_size, pred_key)
 
     def serving_input_receiver_fn(self):
         features = {
+            "label_ids": tf.placeholder(dtype=tf.int64, shape=[None, 1], name='label_ids'),
             "input_ids": tf.placeholder(dtype=tf.int64, shape=[None, self.max_seq_len], name='input_ids'),
             "input_mask": tf.placeholder(dtype=tf.int64, shape=[None, self.max_seq_len], name='input_mask'),
             "segment_ids": tf.placeholder(dtype=tf.int64, shape=[None, self.max_seq_len], name='segment_ids'),
         }
 
         return tf.estimator.export.build_raw_serving_input_receiver_fn(features)
-
+    
     def preprocess_sentences(self, sentences):
         features = {'input_ids': [],
                     'input_mask': [],
-                    'segment_ids': []}
+                    'segment_ids': [],
+                    'label_ids': []}
 
         # Get predictions dictionary
         for i, s in enumerate(sentences):
-            input_ids, input_mask, segment_ids = self.convert_example(
+            input_ids, input_mask, segment_ids, label_ids = self.convert_example(
                 i, s, self.max_seq_len, self.tokenizer)
 
             features['input_ids'].append(input_ids)
             features['input_mask'].append(input_mask)
             features['segment_ids'].append(segment_ids)
+            features['label_ids'].append(label_ids)
 
         return features
 
@@ -67,4 +72,4 @@ class FEProcessor(Processor):
         assert len(input_mask) == max_seq_length
         assert len(segment_ids) == max_seq_length
 
-        return (input_ids, input_mask, segment_ids)
+        return (input_ids, input_mask, segment_ids, [0])
