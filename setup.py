6777b1e701e7a5af289e4cb3d566a0210ce39f19
pinkmoody <133006366+pinkmoody@users.noreply.github.com>
2023-12-23 22:55:27 +0000

    Set version to 0.1

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
-from
+from setuptools import setup, find_packages
+from pkg_resources import DistributionNotFound, get_distribution
+
+
+def get_dist(pkgname):
+    try:
+        return get_distribution(pkgname)
+    except DistributionNotFound:
+        return None
+
+
+install_deps = ['numpy']
+
+if
